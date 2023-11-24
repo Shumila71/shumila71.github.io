9665590609216ad0c535c0a34e062446a1da5ee0
Mark <113240493+Shumila71@users.noreply.github.com>
2023-11-24 20:52:21 +0300

    Add files via upload

--- scripts/showContent.js
+++ scripts/showContent.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
         blocks.forEach(block => {
             let blockPosition = block.getBoundingClientRect().top;
  
-            if (blockPosition < windowHeight - 100) {
+            if (blockPosition < windowHeight - 80) {
                 block.style.opacity = "1";
                 block.style.transform = "translateY(0)";
             }
